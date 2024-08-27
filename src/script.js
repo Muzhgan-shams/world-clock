61a73f65b1653b21502648887d6fbf40782a4935
Muzhgan Shams <muzhgan_shams@yahoo.com>
2024-08-27 18:13:02 +0500

    Add multiple locations functionality

--- src/script.js
+++ src/script.js
@@ -1,22 +1,49 @@
 function updateTime() {
   // Kabul
   let kabulElement = document.querySelector('#kabul');
-  let kabulDateElement = kabulElement.querySelector('.date');
-  let kabulTimeElement = kabulElement.querySelector('.time');
-  let kabulTime = moment().tz('Asia/Kabul');
+  if (kabulElement) {
+    let kabulDateElement = kabulElement.querySelector('.date');
+    let kabulTimeElement = kabulElement.querySelector('.time');
+    let kabulTime = moment().tz('Asia/Kabul');
 
-  kabulDateElement.innerHTML = kabulTime.format('MMMM  Do YYYY');
-  kabulTimeElement.innerHTML = kabulTime.format('h:mm:ss [<small>]A[</small>]');
+    kabulDateElement.innerHTML = kabulTime.format('MMMM  Do YYYY');
+    kabulTimeElement.innerHTML = kabulTime.format(
+      'h:mm:ss [<small>]A[</small>]'
+    );
+  }
   // Peshawar
   let peshawarElement = document.querySelector('#peshawar');
-  let peshawarDateElement = peshawarElement.querySelector('.date');
-  let peshawarTimeElement = peshawarElement.querySelector('.time');
-  let peshawarTime = moment().tz('Asia/Karachi');
+  if (peshawarElement) {
+    let peshawarDateElement = peshawarElement.querySelector('.date');
+    let peshawarTimeElement = peshawarElement.querySelector('.time');
+    let peshawarTime = moment().tz('Asia/Karachi');
 
-  peshawarDateElement.innerHTML = peshawarTime.format('MMMM  Do YYYY');
-  peshawarTimeElement.innerHTML = peshawarTime.format(
-    'h:mm:ss [<small>]A[</small>]'
-  );
+    peshawarDateElement.innerHTML = peshawarTime.format('MMMM  Do YYYY');
+    peshawarTimeElement.innerHTML = peshawarTime.format(
+      'h:mm:ss [<small>]A[</small>]'
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace('_', ' ').split('/')[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector('#cities');
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format('MMMM	Do YYYY')}</div>
+    </div>
+    <div class="time">${cityTime.format('h:mm:ss')} <small>${cityTime.format(
+    'A'
+  )}</small></div>
+  </div>
+  `;
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+let selectElement = document.querySelector('#city');
+selectElement.addEventListener('change', updateCity);
